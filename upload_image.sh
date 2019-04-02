c9ce428c8d07784afd70fa7b5bf1e8b1deba4530
João Correia Lopes <jlopes@fe.up.pt>
2019-04-02 16:43:24 +0100

    Update upload_image.sh

--- upload_image.sh
+++ upload_image.sh
@@ -6,10 +6,10 @@ set -e
 DOCKER_USERNAME=jlopes60   # Replace by your docker hub username
 IMAGE_NAME=demo            # Replace with your group's image name
 
-# the Docker container lbaw_php must be running
-docker exec lbaw_php composer install 
-docker exec lbaw_php php artisan clear-compiled
-docker exec lbaw_php php artisan optimize
+# Ensure that dependencies are available
+composer install
+php artisan clear-compiled
+php artisan optimize
 
 docker build -t $DOCKER_USERNAME/$IMAGE_NAME .
 docker push $DOCKER_USERNAME/$IMAGE_NAME
