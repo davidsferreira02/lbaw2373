dc76513623af1a72033b912cdeac452e25a88e01
David Ferreira <davidferreira@Air-de-David-2.home>
2023-11-17 14:01:08 +0000

    Small changes

--- routes/web.php
+++ routes/web.php
@@ -23,24 +23,7 @@ use App\Http\Controllers\UsersController;
 // Home
 Route::redirect('/', '/login');
 
-// Cards
-Route::controller(CardController::class)->group(function () {
-    Route::get('/cards', 'list')->name('cards');
-    Route::get('/cards/{id}', 'show');
-});
 
-
-// API
-Route::controller(CardController::class)->group(function () {
-    Route::put('/api/cards', 'create');
-    Route::delete('/api/cards/{card_id}', 'delete');
-});
-
-Route::controller(ItemController::class)->group(function () {
-    Route::put('/api/cards/{card_id}', 'create');
-    Route::post('/api/item/{id}', 'update');
-    Route::delete('/api/item/{id}', 'delete');
-});
 
 
 // Authentication
