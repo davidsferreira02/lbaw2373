72da21ba59e80c1819a4acdc1dab317cdf1e5e46
David Ferreira <davidferreira@MacBook-Air-de-David-2.local>
2023-12-05 10:56:28 +0000

    Search problem solved

--- resources/views/pages/search_results.blade.php
+++ resources/views/pages/search_results.blade.php
@@ -1,45 +1,69 @@
 @extends('layouts.app')
 
 @section('content')
-  
-
-<a href="{{ route('project.home') }}" class="btn btn-primary">
-    <i class="fas fa-arrow-left"></i> <!-- Use "fas" para ícones sólidos -->
-    
-</a>
-
+    <a href="{{ route('project.home') }}" class="btn btn-primary">
+        <i class="fas fa-arrow-left"></i>
+    </a>
 
     @if($search)
         <h1>Search results: {{ $search }}</h1>
     @endif
 
-    <h2>Users:</h2>
-    @if(count($users) > 0)
-        <ul>
-            @foreach($users as $user)
+    <div class="row">
+        <div class="col-sm-6">
+            <button class="btn btn-primary active" onclick="filterResults('users')">Users</button>
+            <ul id="usersList" style="display: block;">
+                <h2>Users:</h2>
+                @if(count($users) > 0)
+                    @foreach($users as $user)
+                        <li>
+                            <button class="btn btn-primary" onclick="location.href='{{ route('profile', ['id' => $user->id,'project' => $user->projectMember()]) }}'">
+                                {{ $user->name }}
+                            </button>
+                        </li>
+                    @endforeach
+                @else
+                    <p>No users found.</p>
+                @endif
+            </ul>
+        </div>
+        <div class="col-sm-6">
+            <button class="btn btn-primary" onclick="filterResults('projects')">Projects</button>
+            <ul id="projectsList" style="display: none;">
+                <h2>Projects:</h2>
+                @if(count($projects) > 0)
+                    @foreach($projects as $project)
+                        <li>
+                            <button class="btn btn-primary" onclick="location.href='{{ route('project.show', ['title' => $project->title]) }}'">
+                                {{ $project->title }}
+                            </button>
+                        </li>
+                    @endforeach
+                @else
+                    <p>No projects found.</p>
+                @endif
+            </ul>
+        </div>
+    </div>
 
-                <button class="btn btn-primary" onclick="location.href='{{ route('profile', ['id' => $user->id,'project' => $user->projectMember()]) }}'">
-                    {{ $user->name }}
-                </button>
-            @endforeach
-        </ul>
-    @else
-        <p>No users found.</p>
-    @endif
+    <script>
+        function filterResults(type) {
+            const usersList = document.getElementById('usersList');
+            const projectsList = document.getElementById('projectsList');
+            const usersButton = document.querySelector('[onclick="filterResults(\'users\')"]');
+            const projectsButton = document.querySelector('[onclick="filterResults(\'projects\')]');
 
-    <h2>Projects:</h2>
-    @if(count($projects) > 0)
-        <ul>
-            @foreach($projects as $project)
-            <button class="btn btn-primary" onclick="location.href='{{ route('project.show', ['title' => $project->title]) }}'">
-                {{ $project->title }}
-            </button>
-               
-            @endforeach
-        </ul>
-    @else
-        <p>No projects found.</p>
-    @endif
-
-    <a href="{{ route('project.home') }}" class="btn btn-primary">Go Home</a>
+            if (type === 'users') {
+                usersList.style.display = 'block';
+                projectsList.style.display = 'none';
+                usersButton.classList.add('active');
+                projectsButton.classList.remove('active');
+            } else if (type === 'projects') {
+                usersList.style.display = 'none';
+                projectsList.style.display = 'block';
+                usersButton.classList.remove('active');
+                projectsButton.classList.add('active');
+            }
+        }
+    </script>
 @endsection
