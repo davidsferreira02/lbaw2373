608efd1e42d34a9029650a28f8f223164d46fce8
Sofia Teixeira <up201806629@fe.up.pt>
2023-12-20 10:49:38 +0000

    Fix pending invitations

--- resources/views/pages/pedingInvites.blade.php
+++ resources/views/pages/pedingInvites.blade.php
@@ -1,29 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-<a href="{{ route('project.home') }}" class="btn btn-primary">
-    <i class="fas fa-arrow-left"></i> <!-- Use "fas" para ícones sólidos -->
-    
-</a>
+    <a href="{{ route('project.home') }}" class="btn btn-primary">
+        <i class="fas fa-arrow-left"></i>
+    </a>
     <h1>Pending invitations</h1>
 
     @if ($pendingInvites->isEmpty())
         <p>No invitations pending at the moment.</p>
     @else
-        <ul>
+        <div class="projects-container">
             @foreach ($pendingInvites as $invite)
-                <li>
-                    There is a pending invitation to the project: {{ $invite->project->title }}
-                    <form action="{{ route('accept.invite', ['id_user' => $invite->id_user, 'id_project' => $invite->id_project]) }}" method="POST">
-                        @csrf
-                        <button type="submit"  onclick="return confirm('Tem certeza que deseja aceitar este convite?')">Accept</button>
-                    </form>
-                    <form action="{{ route('decline.invite', ['id_user' => $invite->id_user, 'id_project' => $invite->id_project]) }}" method="POST">
-                        @csrf
-                        <button type="submit"  onclick="return confirm('Tem certeza que deseja recusar este convite?')">Refuse</button>
-                    </form>
-                </li>
+                <div class="project-square">
+                    <p>Project: {{ $invite->project->title }}</p>
+                    <div class="button-container">
+                        <form action="{{ route('accept.invite', ['id_user' => $invite->id_user, 'id_project' => $invite->id_project]) }}" method="POST">
+                            @csrf
+                            <button type="submit" onclick="return confirm('Tem certeza que deseja aceitar este convite?')">Accept</button>
+                        </form>
+                        <form action="{{ route('decline.invite', ['id_user' => $invite->id_user, 'id_project' => $invite->id_project]) }}" method="POST">
+                            @csrf
+                            <button type="submit" onclick="return confirm('Tem certeza que deseja recusar este convite?')">Refuse</button>
+                        </form>
+                    </div>
+                </div>
             @endforeach
-        </ul>
+        </div>
     @endif
 @endsection
