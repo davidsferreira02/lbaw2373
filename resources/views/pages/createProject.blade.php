3cb4e4cb9a0ad88e1035f3587551597d8dcfe173
Sofia Teixeira <up201806629@fe.up.pt>
2023-12-20 11:09:22 +0000

    Fix create project and create task

--- resources/views/pages/createProject.blade.php
+++ resources/views/pages/createProject.blade.php
@@ -2,47 +2,46 @@
 
 @section('content')
 <a href="{{ route('project.home') }}" class="btn btn-primary">
-    <i class="fas fa-arrow-left"></i> <!-- Use "fas" para ícones sólidos -->
-    
+    <i class="fas fa-arrow-left"></i>
 </a>
 
-    <div class="container">
-        <h1>Create a New Project</h1>
+<div class="container square-container">
+    <h1>Create a New Project</h1>
 
-        @if(session('success'))
-            <div class="alert alert-success">
-                {{ session('success') }}
-            </div>
-        @endif
-
-        <form method="post" action="{{ route('project.store') }}">
-            @csrf
-
-            <div class="form-group">
-                <label for="title">Project Title</label>
-                <input type="text" id="title" name="title" class="form-control">
-                <span class="error">
-                    {{ $errors->first('title') }}
-                  </span>
-            </div>
-
-            <div class="form-group">
-                <label for="description">Project Description</label>
-                <textarea id="description" name="description" class="form-control"></textarea>
-                <span class="error">
-                    {{ $errors->first('description') }}
-                  </span>
-            </div>
-
-            <div class="form-group">
-                <label for="theme">Project Theme</label>
-                <textarea id="theme" name="theme" class="form-control"></textarea>
-                <span class="error">
-                    {{ $errors->first('theme') }}
-                  </span>
-            </div>
-            <button type="submit"href="{{ route('project.index') }}" class="btn btn-primary">Create Project</button>
-        </form>
+    @if(session('success'))
+    <div class="alert alert-success">
+        {{ session('success') }}
     </div>
+    @endif
 
+    <form method="post" action="{{ route('project.store') }}">
+        @csrf
+
+        <div class="form-group">
+            <label for="title">Project Title</label>
+            <input type="text" id="title" name="title" class="form-control">
+            <span class="error">
+                {{ $errors->first('title') }}
+            </span>
+        </div>
+
+        <div class="form-group">
+            <label for="description">Project Description</label>
+            <textarea id="description" name="description" class="form-control"></textarea>
+            <span class="error">
+                {{ $errors->first('description') }}
+            </span>
+        </div>
+
+        <div class="form-group">
+            <label for="theme">Project Theme</label>
+            <textarea id="theme" name="theme" class="form-control"></textarea>
+            <span class="error">
+                {{ $errors->first('theme') }}
+            </span>
+        </div>
+
+        <button type="submit" href="{{ route('project.index') }}" class="btn btn-primary">Create Project</button>
+    </form>
+</div>
 @endsection
